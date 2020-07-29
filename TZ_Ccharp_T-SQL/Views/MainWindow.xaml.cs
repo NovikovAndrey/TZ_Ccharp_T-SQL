e1875ca0449dae0f3bc171bf53db5d953f12ce3a
SAT <novikov.andrey.v@hotmail.com>
2020-07-29 22:47:10 +0300

    Main work

---
+++ TZ_Ccharp_T-SQL/Views/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TZ_Ccharp_T_SQL.Controller;
+
+namespace TZ_Ccharp_T_SQL.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string FilePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FilePath = HomeController.GetFile();
+            if (FilePath != null)
+            {
+                FilePathTextBox.Text = FilePath;
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultsTextBox.Text = "";
+            var homeController = new HomeController();
+            var ListResults = homeController.SearchRequest(FilePath, XPathTextBox.Text);
+            foreach (string vs in ListResults)
+            {
+                ResultsTextBox.Text += vs;
+            }
+        }
+    }
+}
